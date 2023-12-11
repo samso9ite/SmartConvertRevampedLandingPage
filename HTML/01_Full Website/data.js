53d01532b5974723798b06ed4baf5ddc3fe96449
Samso9ite <samso9ite@gmail.com>
2023-12-11 12:54:27 +0100

    first commit

---
+++ HTML/01_Full Website/data.js
@@ -0,0 +1,49 @@
+
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        testimonials: [],
+        testimony: {}
+    },
+
+    methods: {
+       async  getTestimony(){
+            const res = await fetch('https://api.smartconvert.ng/api/v1/list-testimonials')
+            let data = await res.text()
+            let parsedData = JSON.parse(data)
+            let testimonials = []
+            parsedData.map(item => {
+                if(item.status == true ){
+                  testimonials.push(item)  
+                }
+            })
+            this.testimony = testimonials[0]
+        },
+    },
+    mounted() {
+        this.getTestimony()
+    },
+});
+
+var testimonial = new Vue({ 
+    el: '#testimonial',
+    data: {
+        testimonials: [],
+    },
+
+    methods: {
+       async  getTestimonials(){
+            const res = await fetch('https://api.smartconvert.ng/api/v1/list-testimonials')
+            let data = await res.text()
+            let parsedData = JSON.parse(data)
+            parsedData.map(item => {
+                if(item.status == true ){
+                  this.testimonials.push(item)  
+                }
+            })
+        },
+    },
+    mounted() {
+        this.getTestimonials()
+    },
+});
